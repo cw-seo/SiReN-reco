f4b437093921b0072a60757cc45d75ad084f5592
cw.seo <changwoni01@gmail.com>
2021-10-09 12:57:31 +0900

    normalized update

--- convols.py
+++ convols.py
@@ -6,19 +6,23 @@ import torch
 from torch_geometric.nn import MessagePassing
 from torch_geometric.utils import add_self_loops
     
-    
-class LightGConv(MessagePassing): 
-    def __init__(self, in_channels, out_channels):
-        super(LightGConv, self).__init__(aggr='mean')  
-
-    def forward(self, x, edge_index):
-        # edge_index, _ = add_self_loops(edge_index, num_nodes=x.size(0))
-        return self.propagate(edge_index, x=x)
+class LightGConv(MessagePassing):
+    def __init__(self):
+        super().__init__(aggr='add')
         
-    def message(self, x_j): 
-        return  x_j
+    def forward(self,x,edge_index):
+        
+        row, col = edge_index
+        deg = degree(col, x.size(0), dtype=x.dtype)
+        deg_inv_sqrt = deg.pow(-0.5)
+        deg_inv_sqrt[deg_inv_sqrt == float('inf')] = 0
+        norm = deg_inv_sqrt[row] * deg_inv_sqrt[col]
+        
+        return self.propagate(edge_index, x=x, norm=norm)
     
-    def update(self, inputs: Tensor) -> Tensor: # Gamma
+    def message(self,x_j,norm):
+        return norm.view(-1,1) * x_j
+    def update(self,inputs: Tensor) -> Tensor:
         return inputs
 
 
